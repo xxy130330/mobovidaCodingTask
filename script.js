3c118a3c51167cd1f494f9c1ee839b0e4aebd543
xxy130330 <yangxue0708@gmail.com>
2018-03-15 21:46:49 -0700

    created ajax call to render json

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 $(document).ready(initializeApp);
+
+function initializeApp() {
+    handleAPI();
+}
+
+function handleAPI() {
+    $.ajax({
+        method: 'get',
+        // data: ,
+        url: 'https://www.wirelessemporium.com/products.json',
+        success: function (data) {
+            var productsList = data['products'];
+            console.log(productsList);
+        },
+        error: function () {
+        }
+    })
+}
