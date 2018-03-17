d2b940559db703a2717e768786107208f6a11986
xxy130330 <yangxue0708@gmail.com>
2018-03-17 01:25:42 -0700

    fixing unexpected err

--- script.js
+++ script.js
@@ -134,30 +134,38 @@ function sortSuggested() {
 function sortLowToHigh() {
     $('.listContainer').empty();
     productObject.showItems.sort(function (a, b) {
-        if(a[0]){
+        if (a[0]) {
             return parseFloat(a[0]['attributes'][2]['value']) - parseFloat(b[0]['attributes'][2]['value'])
-        }else {
+        } else {
             return parseFloat(a['attributes'][2]['value']) - parseFloat(b['attributes'][2]['value'])
         }
     });
     $('.listContainer').append(productObject.showItems);
-    // noShowItemErrMsg();
-}
 
 
-function sortHighToLow() {
-    $('.listContainer').empty();
-    productObject.showItems.sort(function (a, b) {
-        if(a[0]){
-            return parseFloat(b[0]['attributes'][2]['value']) - parseFloat(a[0]['attributes'][2]['value'])
-        }else {
-            return parseFloat(b['attributes'][2]['value']) - parseFloat(a['attributes'][2]['value'])
-        }
-    });
-    $('.listContainer').append(productObject.showItems);
-    // noShowItemErrMsg();
+    // var x = document.getElementsByClassName("filterDiv");
+    // // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    // for (i = 0; i < x.length; i++) {
+    //     removeClass(x[i], "show");
+    // }
+
+
+    function sortHighToLow() {
+        $('.listContainer').empty();
+        productObject.showItems.sort(function (a, b) {
+            if (a[0]) {
+                return parseFloat(b[0]['attributes'][2]['value']) - parseFloat(a[0]['attributes'][2]['value'])
+            } else {
+                return parseFloat(b['attributes'][2]['value']) - parseFloat(a['attributes'][2]['value'])
+            }
+        });
+        // $('.listContainer').append(productObject.showItems);
+        $('.listContainer').append(productObject.wholeList);
+        for (var a = 0; a < productObject.showItems.length; a++) {
+            productObject.showItems[a].attr('show');
+        }    // noShowItemErrMsg();
 //?
-}
+    }
 
 // function checkIfShowItemsExist() {
 //     if(!productObject.showItems){
@@ -175,61 +183,69 @@ function sortHighToLow() {
 //     }).appendTo($('.listContainer'));
 // }
 
-function noShowItemErrMsg() {
-    if(productObject.showItems.length === 0) {
-        $('.listContainer').text('Sorry, there\'s match item!');
+    function noShowItemErrMsg() {
+        if (productObject.showItems.length === 0) {
+            $('.listContainer').text('Sorry, there\'s match item!');
+        }
     }
-}
+
 // **************************** Project section filter menu *****************
 
-filterSelection("all");
-function filterSelection(c) {
-    var x,i;
-    productObject.showItems = [];
-    // x = productObject.wholeList;
-    x = document.getElementsByClassName("filterDiv");
-    if (c === "all") {
-        c = "";
-        productObject.showItems = productObject.wholeList;
-    }
-    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-    for (i = 0; i < x.length; i++) {
-        removeClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) {
-            toggleClass(x[i], "show");
-            // if(productObject.showItems.indexOf(x[i]) === -1) {
-                productObject.showItems.push(x[i]);
-            // }
-            // productObject.showItems[i] = x[i];
+    filterSelection("all");
 
+    function filterSelection(c) {
+        var x, i;
+        productObject.showItems = [];
+        // x = productObject.wholeList;
+        x = document.getElementsByClassName("filterDiv");
+        if (c === "all") {
+            c = "";
+            // for (i = 0; i < x.length; i++) {
+            //     removeClass(x[i], "show");
+            //     if (x[i].className.indexOf(c) > -1) {
+            //         toggleClass(x[i], "show");
+            //     }
+            // }
+            productObject.showItems = productObject.wholeList;
+        }
+        // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+        for (i = 0; i < x.length; i++) {
+            removeClass(x[i], "show");
+            if (x[i].className.indexOf(c) > -1) {
+                toggleClass(x[i], "show");
+                // if(productObject.showItems.indexOf(x[i]) === -1) {
+                productObject.showItems.push(x[i]);
+                // }
+                // productObject.showItems[i] = x[i];
+
+            }
         }
     }
-}
 
 // Show filtered elements
-function toggleClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {
-            element.className += " " + arr2[i];
+    function toggleClass(element, name) {
+        var i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+            if (arr1.indexOf(arr2[i]) == -1) {
+                element.className += " " + arr2[i];
+            }
         }
     }
-}
 
 // Hide elements that are not selected
-function removeClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
+    function removeClass(element, name) {
+        var i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+            while (arr1.indexOf(arr2[i]) > -1) {
+                arr1.splice(arr1.indexOf(arr2[i]), 1);
+            }
         }
+        element.className = arr1.join(" ");
     }
-    element.className = arr1.join(" ");
-}
 
 // Add active class to the current control button (highlight it)
 //
@@ -237,15 +253,16 @@ function removeClass(element, name) {
 // var productContainer = bodyContainer.getElementsByClassName('container');
 // var btnContainer = productContainer.getElementById("btnContainer");
 // var btns = btnContainer.getElementsByClassName("filterBtn");
-var btns = $('.filterBtn');
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = $('.activeBtn');
-        // var current = document.getElementsByClassName("activeBtn");
-        // current.toggleClass('activeBtn');
-        current[0].className = current[0].className.replace(" activeBtn", "");
-        this.className = "filterBtn activeBtn";
-    });
+    var btns = $('.filterBtn');
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            var current = $('.activeBtn');
+            // var current = document.getElementsByClassName("activeBtn");
+            // current.toggleClass('activeBtn');
+            current[0].className = current[0].className.replace(" activeBtn", "");
+            this.className = "filterBtn activeBtn";
+        });
+    }
 }
 
 
